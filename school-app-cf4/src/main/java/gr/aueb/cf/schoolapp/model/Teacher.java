59f8b15dabf36419391d7a5533795dd23726a66f
dmastoras68 <mastoras.demetris@hotmail.gr>
2023-07-12 20:43:33 +0300

    com1

---
+++ school-app-cf4/src/main/java/gr/aueb/cf/schoolapp/model/Teacher.java
@@ -0,0 +1,71 @@
+package gr.aueb.cf.schoolapp.model;
+
+import java.util.Objects;
+
+public class Teacher {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    public Teacher() {}
+
+    public Teacher(Integer id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Teacher teacher = (Teacher) o;
+
+        if (!Objects.equals(id, teacher.id)) return false;
+        if (!Objects.equals(firstName, teacher.firstName)) return false;
+        return Objects.equals(lastName, teacher.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        return result;
+    }
+}
